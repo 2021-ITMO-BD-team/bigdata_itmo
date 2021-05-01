d94490a291a330a2921c2e149483a90ee06d4561
Miffka <miffka1992@inbox.ru>
2021-05-01 18:15:21 +0300

    [feature] Add audio extraction script
    * Use moviepy with ffmpeg to read and write files

---
+++ bigdata_itmo/video_files/extract_audio.py
@@ -0,0 +1,48 @@
+import argparse
+import glob
+import os
+import os.path as osp
+
+import moviepy.editor as mp
+import tqdm
+
+from bigdata_itmo.config import system_config
+
+
+def create_parser():
+    parser = argparse.ArgumentParser("Convert video to audio")
+
+    parser.add_argument(
+        "--input_path",
+        default=osp.join(system_config.data_dir, "raw", "pytube_data", "BBC_News"),
+        help="File or folder with video files to extract audio",
+    )
+    parser.add_argument(
+        "--output_folder",
+        default=osp.join(system_config.data_dir, "raw", "audio_data", "BBC_news"),
+        help="Folder to store audio files",
+    )
+    parser.add_argument("--audio_format", default="wav", help="Format of output audio file")
+
+    args = parser.parse_args()
+    return args
+
+
+def extract_one_file(fpath, output_folder, format="wav"):
+    os.makedirs(output_folder, exist_ok=True)
+
+    my_clip = mp.VideoFileClip(fpath)
+    out_fpath = osp.join(output_folder, f"{osp.splitext(osp.basename(fpath))[0]}.{format}")
+    my_clip.audio.write_audiofile(out_fpath)
+
+
+if __name__ == "__main__":
+    args = create_parser()
+
+    if osp.isfile(args.input_path):
+        extract_one_file(args.input_path, args.output_folder)
+
+    else:
+        file_list = glob.glob(osp.join(args.input_path, "*"))
+        for fpath in tqdm.tqdm(file_list, desc="Extracting audios"):
+            extract_one_file(fpath, args.output_folder)
