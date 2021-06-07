4fa364e0fd788059fc673de7c5c9f16245b4b52c
Miffka <miffka1992@inbox.ru>
2021-06-07 21:27:38 +0300

    [refactor] Delete Kafka folders

--- bigdata_itmo/kafka/producer.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from kafka import KafkaProducer
-
-producer = KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=str.encode)
-
-for i in range(100000):
-    producer.send("test", str(i))
-    time.sleep(1)
