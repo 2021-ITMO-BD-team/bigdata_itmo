8c9eda9be2d8592b3ca3390a56c10966344b9a74
Miffka <miffka1992@inbox.ru>
2021-06-08 22:41:37 +0300

    [refactor] Apply style tests to stream reader

--- bigdata_itmo/stream_reader/stream_reader.py
+++ bigdata_itmo/stream_reader/stream_reader.py
@@ -1,57 +1,62 @@
-from streamlink import streams
-import ffmpeg
-from kafka import KafkaProducer
-import fire
-from bigdata_itmo.config import system_config, kafka_config
-from os import path, remove
 import time
+from os import path, remove
 from sys import exit
+
+import ffmpeg
+import fire
+from kafka import KafkaProducer
+from streamlink import streams
+
+from bigdata_itmo.config import kafka_config, system_config
 
 filedir = path.join(system_config.data_dir, "internal")
 
+
 def remove_files():
     try:
-        for t in range(1,13):
-            temp_path = path.join(filedir, 'part{}.ts'.format(t))
+        for t in range(1, 13):
+            temp_path = path.join(filedir, "part{}.ts".format(t))
             remove(temp_path)
-        temp_path = path.join(filedir, 'temp.wav')
+        temp_path = path.join(filedir, "temp.wav")
         remove(temp_path)
-    except:
-        print('Error while removing temporary files\n')
+    except:  # noqa E722
+        print("Error while removing temporary files\n")
         exit(1)
+
 
 def send_file(producer, stream_link):
     timestamp = time.time()
     try:
-        with open('temp.wav', 'rb') as f:
+        with open("temp.wav", "rb") as f:
             bytes = f.read()
-    except:
-        print('Error while reading audiofile\n')
+    except:  # noqa E722
+        print("Error while reading audiofile\n")
         exit(1)
     try:
-        producer.send('{}_{}'.format(timestamp, stream_link), bytes)
-    except:
-        print('Error while sending file\n')
+        producer.send("{}_{}".format(timestamp, stream_link), bytes)
+    except:  # noqa E722
+        print("Error while sending file\n")
         exit(1)
+
 
 def stream_reciever(producer, stream_link):
     stream = streams(stream_link)
-    fd = stream['worst'].open()
+    fd = stream["worst"].open()
     i = 1
     try:
         while True:
             if i > 12:
                 inputs_audio = []
                 for t in range(1, 13):
-                    temp_path = path.join(filedir, 'part{}.ts'.format(t))
+                    temp_path = path.join(filedir, "part{}.ts".format(t))
                     inputs_audio.append(ffmpeg.input(temp_path).audio)
-                temp_path = path.join(filedir, 'temp.wav')
+                temp_path = path.join(filedir, "temp.wav")
                 ffmpeg.concat(*inputs_audio, v=0, a=1).output(temp_path).run(capture_stderr=True)
                 send_file(producer, stream_link)
                 remove_files()
                 i = 1
             data = fd.read(2500000 * 6)
-            with open('part{}.ts'.format(i), 'wb+') as f:
+            with open("part{}.ts".format(i), "wb+") as f:
                 f.write(data)
             i += 1
     except ffmpeg.Error as err:
@@ -59,17 +64,19 @@ def stream_reciever(producer, stream_link):
         fd.close()
         exit(1)
 
+
 def start_reading(stream_link=None):
     if stream_link is not None:
         try:
             producer = KafkaProducer(bootstrap_servers=kafka_config.bootstrap_server)
-        except:
-            print('Error while running producer\n')
+        except:  # noqa E722
+            print("Error while running producer\n")
             exit(1)
         stream_reciever(producer, stream_link)
     else:
-        print('Enter a stream link with -stream_link\n')
+        print("Enter a stream link with -stream_link\n")
         exit(1)
 
-if __name__ == 'main':
+
+if __name__ == "main":
     fire.Fire(start_reading)
